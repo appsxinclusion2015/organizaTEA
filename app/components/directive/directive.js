16855e56b19ca5d49bc23b445e65e4eb02f105e5
emravera <ravera.emanuel@gmail.com>
2015-09-10 21:12:47 -0300

    Changed styles and added logo of the app

--- app/components/directive/directive.js
+++ app/components/directive/directive.js
@@ -7,6 +7,7 @@ angular.module('myApp.directive', [])
     restrict: 'A',
     link: function (scope, elem, attrs) {
       elem.attr('class','mdl-button mdl-js-button mdl-button--fab mdl-button--colored app-back-button');
+      elem.attr('style', 'background: #F17408;');
       elem.append('<i class="material-icons">arrow_back</i>');
       
       elem.bind('click', function () {
