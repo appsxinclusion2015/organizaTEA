9bd2d2fc68a046ef9aa67e869a432940bd909182
Nicolas Oliver <nicolasoliver03@gmail.com>
2016-09-03 18:52:54 -0300

    allow edit video source in resource
    there is a problem with back button thought

--- app/view/resource/resource.js
+++ app/view/resource/resource.js
@@ -10,20 +10,34 @@ angular.module('myApp.resourceView', ['ngRoute', 'ngAnimate'])
 
   $scope.form = null;
 
+  $scope.searchQuery = null;
+
+  $scope.previewUrl = null;
+
+  $scope.searchResults = [];
+
   $scope.reset = function () {
     //Initialize the routine selected
     $scope.model = $rootScope.initializeResource(resourceId);
 
+    $scope.searchQuery = null;
+
+    $scope.searchResults = [];
+
+    $scope.previewUrl = null;
+
     if (!$scope.model) return;
 
     $scope.form = {
-      title: $scope.model.resource.title
+      title: $scope.model.resource.title,
+      src: $scope.model.resource.src
     };
   };
 
   $scope.updateResource = function () {
     $scope.model.resource.title = $scope.form.title;
-    
+    $scope.model.resource.src = $scope.form.src;
+
     $rootScope.updateResource($scope.model.resource);
     $scope.reset();
   };
@@ -31,6 +45,23 @@ angular.module('myApp.resourceView', ['ngRoute', 'ngAnimate'])
   $scope.deleteResource = function () {
     $rootScope.deleteResource(resourceId);
     $scope.model = null;
+  };
+
+  $scope.searchResources = function () {
+    var request = gapi.client.youtube.search.list({
+      part: 'snippet',
+      type: 'video',
+      q: $scope.searchQuery,
+    });
+
+    request.execute(function (data) {
+      $scope.searchResults = data.items;
+      $scope.$digest();
+    });
+  };
+  
+  $scope.previewResource = function (resource) {
+    $scope.form.src = 'https://www.youtube.com/embed/' + resource.id.videoId;
   };
 
   $scope.reset();
